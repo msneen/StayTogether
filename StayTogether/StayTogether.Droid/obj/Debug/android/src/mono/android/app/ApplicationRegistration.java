06d2f16ebd98bd75af17fd39b9f01399ef484906
msneen <michael.sneen@gmail.com>
2016-10-21 17:23:02 -0700

    Added Token to Hub Connection for Security

--- StayTogether/StayTogether.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
+++ StayTogether/StayTogether.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,7 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
-		mono.android.Runtime.register ("StayTogether.Droid.MainApplication, StayTogether.Droid, Version=1.0.6138.27822, Culture=neutral, PublicKeyToken=null", md510f33033d97d658d0fc8608b4f864abf.MainApplication.class, md510f33033d97d658d0fc8608b4f864abf.MainApplication.__md_methods);
+		mono.android.Runtime.register ("StayTogether.Droid.MainApplication, StayTogether.Droid, Version=1.0.6138.30764, Culture=neutral, PublicKeyToken=null", md5efb945b341085020e059b674803bdd89.MainApplication.class, md5efb945b341085020e059b674803bdd89.MainApplication.__md_methods);
 		
 	}
 }
--- StayTogether/StayTogether/Location/LocationSender.cs
+++ StayTogether/StayTogether/Location/LocationSender.cs
@@ -37,8 +37,8 @@ namespace StayTogether
 	    public void InitializeSignalRAsync()
         {
             // Connect to the server
-            //_hubConnection = new HubConnection("http://76.167.114.111/StayTogetherServer/");//jeff
             _hubConnection = new HubConnection("http://162.231.59.41/StayTogetherServer/");//mike
+            _hubConnection.Headers.Add("AuthToken", "x0y2!tJyHR%$Sip@*%amaGxvs");
 
             // Create a proxy to the 'ChatHub' SignalR Hub
             _chatHubProxy = _hubConnection.CreateHubProxy("StayTogetherHub");
