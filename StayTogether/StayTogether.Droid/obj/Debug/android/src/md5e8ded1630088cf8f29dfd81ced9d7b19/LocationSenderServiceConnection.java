7745a511bf1080847c9c87fbfb8b1810b825b258
msneen <michael.sneen@gmail.com>
2016-10-21 17:55:00 -0700

    Modifed Security Attribute

---
+++ StayTogether/StayTogether.Droid/obj/Debug/android/src/md5e8ded1630088cf8f29dfd81ced9d7b19/LocationSenderServiceConnection.java
@@ -0,0 +1,64 @@
+package md5e8ded1630088cf8f29dfd81ced9d7b19;
+
+
+public class LocationSenderServiceConnection
+	extends java.lang.Object
+	implements
+		mono.android.IGCUserPeer,
+		android.content.ServiceConnection
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onServiceConnected:(Landroid/content/ComponentName;Landroid/os/IBinder;)V:GetOnServiceConnected_Landroid_content_ComponentName_Landroid_os_IBinder_Handler:Android.Content.IServiceConnectionInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onServiceDisconnected:(Landroid/content/ComponentName;)V:GetOnServiceDisconnected_Landroid_content_ComponentName_Handler:Android.Content.IServiceConnectionInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("StayTogether.Droid.Services.LocationSenderServiceConnection, StayTogether.Droid, Version=1.0.6138.31773, Culture=neutral, PublicKeyToken=null", LocationSenderServiceConnection.class, __md_methods);
+	}
+
+
+	public LocationSenderServiceConnection () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == LocationSenderServiceConnection.class)
+			mono.android.TypeManager.Activate ("StayTogether.Droid.Services.LocationSenderServiceConnection, StayTogether.Droid, Version=1.0.6138.31773, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	public LocationSenderServiceConnection (md5ac32afa8748cdeb1e50c8df751be1dbe.MainActivity p0) throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == LocationSenderServiceConnection.class)
+			mono.android.TypeManager.Activate ("StayTogether.Droid.Services.LocationSenderServiceConnection, StayTogether.Droid, Version=1.0.6138.31773, Culture=neutral, PublicKeyToken=null", "StayTogether.Droid.Activities.MainActivity, StayTogether.Droid, Version=1.0.6138.31773, Culture=neutral, PublicKeyToken=null", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onServiceConnected (android.content.ComponentName p0, android.os.IBinder p1)
+	{
+		n_onServiceConnected (p0, p1);
+	}
+
+	private native void n_onServiceConnected (android.content.ComponentName p0, android.os.IBinder p1);
+
+
+	public void onServiceDisconnected (android.content.ComponentName p0)
+	{
+		n_onServiceDisconnected (p0);
+	}
+
+	private native void n_onServiceDisconnected (android.content.ComponentName p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
--- StayTogether/StayTogether.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
+++ StayTogether/StayTogether.Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,7 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
-		mono.android.Runtime.register ("StayTogether.Droid.MainApplication, StayTogether.Droid, Version=1.0.6138.30764, Culture=neutral, PublicKeyToken=null", md5efb945b341085020e059b674803bdd89.MainApplication.class, md5efb945b341085020e059b674803bdd89.MainApplication.__md_methods);
+		mono.android.Runtime.register ("StayTogether.Droid.MainApplication, StayTogether.Droid, Version=1.0.6138.31992, Culture=neutral, PublicKeyToken=null", md5ad3320a157c65d779739049af294b08b.MainApplication.class, md5ad3320a157c65d779739049af294b08b.MainApplication.__md_methods);
 		
 	}
 }
